31477453ec2ba60034fe70d379e276dffadc055a
thizeh <thiago02_08@hotmail.com>
2022-08-18 16:24:41 -0300

    interaçõs com component trees

--- src/index.js
+++ src/index.js
@@ -5,12 +5,26 @@ import { FaStar } from "react-icons/fa";
 
 const createArray = (length) => [...Array(length)];
 
-function Star({ selected = false }) {
-  return <FaStar color={selected ? "red" : "grays"} />;
+function Star({ selected = false, onSelect }) {
+  return <FaStar color={selected ? "red" : "gray"} onClick={onSelect} />;
 }
 
 function StarRating({ totalStars = 5 }) {
-  return createArray(totalStars).map((n, i) => <Star key={i} />);
+  const [selectedStars, setSelectedStars] = useState(0);
+  return (
+    <>
+      {createArray(totalStars).map((n, i) => (
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
+      ))}
+      <p>
+        {selectedStars} of {totalStars}
+      </p>
+    </>
+  );
 }
 
 function App() {
