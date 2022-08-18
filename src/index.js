bcf62850f12d219dae5e8a2ad0904d014d3ae071
thizeh <thiago02_08@hotmail.com>
2022-08-18 15:40:17 -0300

    checkbom com useState

--- src/index.js
+++ src/index.js
@@ -13,12 +13,16 @@ import "./index.css";
 //Adicionando state ao app component
 
 function App() {
-  const [status, setStatus] = useState("Not Delivered");
+  const [checked, setChecked] = useState(false);
 
   return (
     <div>
-      <h1>The package is: {status}.</h1>
-      <button onClick={() => setStatus("Delivered")}>Deliver</button>
+      <input
+        type="checkbox"
+        value={checked}
+        onChange={() => setChecked((checked) => !checked)}
+      />
+      <p>{checked ? "checked" : "not checked"}</p>
     </div>
   );
 }
