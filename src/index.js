9ff8ff3e530d4c314d482973ad305859f1efc30a
thizeh <thiago02_08@hotmail.com>
2022-08-18 15:34:01 -0300

    Adicionando state ao app component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 
-const [first, second, third] = ["Alex", "Ali", "Anna"];
+// const [first, second, third] = ["Alex", "Ali", "Anna"];
 
-console.log(first, second);
-console.log(second);
-console.log(third);
+// console.log(first, second);
+// console.log(second);
+// console.log(third);
+
+// <App name="Alex" />
+
+//Adicionando state ao app component
+
+function App() {
+  const [status, setStatus] = useState("Not Delivered");
+
+  return (
+    <div>
+      <h1>The package is: {status}.</h1>
+      <button onClick={() => setStatus("Delivered")}>Deliver</button>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App name="Alex" />
+    <App />
   </React.StrictMode>
 );
